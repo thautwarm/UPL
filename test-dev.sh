82e070412c62d2058a358d1a1e44de1926c89af0
thautwarm <twshere@outlook.com>
2020-08-29 13:44:54 +0900

    more examinations with comparisons provided by quick look

--- test-dev.sh
+++ test-dev.sh
@@ -1,4 +1,5 @@
 cd examples
-mlfsc prim2.mlfs --name prim2 --o ./out-prim2
-mlfsc idss.mlfs --sigs "./out-prim2/prim2.mlfsa" --name idss --o ./out-idss
+# mlfsc prim2.mlfs --name prim2 --o ./out-prim2
+# mlfsc idss.mlfs --sigs "./out-prim2/prim2.mlfsa" --name idss --o ./out-idss
+mlfsc icfp2020.mlfs --name icfp2020 --o ./out-icfp2020
 cd ..
